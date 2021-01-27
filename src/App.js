885af119b8f0bb3904b2b9a0fd8c1017c021fd74
Kyle G <ssjkyle32@gmail.com>
2021-01-26 23:21:21 -0800

    Adding Landing PAge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
+import Typist from 'react-typist'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="LandingPage-NameBox">
+          <h1 className="LandingPage-Name" style={{ color: '#fffff0'}}>Kyle Gloria</h1>
+          <h1 className="LandingPage-Name">Kyle Gloria</h1>
+          <h1 className="LandingPage-Name" style={{ color: '#ffffe0' }}>Kyle Gloria</h1>
+        </div>
+        <Typist>        
+          <p>
+          Full Stack Developer | IT Professional
+          </p>
+        </Typist>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
